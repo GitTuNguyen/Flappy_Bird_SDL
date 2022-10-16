6a6b1696243d330f1838cb9aecda7aec4eaba010
Tu Nguyen <tunguyen.96lol@gmail.com>
2022-10-16 19:28:17 +0700

    Update android-project following SDL

--- android-project/app/jni/src/Android.mk
+++ android-project/app/jni/src/Android.mk
@@ -9,9 +9,17 @@ SDL_PATH := ../SDL
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 
 # Add your application source files here...
-LOCAL_SRC_FILES := YourSourceHere.c
+LOCAL_SRC_FILES := ../../../../Source/Main.cpp \
+                   ../../../../Source/Game.cpp \
+                   ../../../../Source/Renderer.cpp \
+                   ../../../../Source/InputManager.cpp \
+                   ../../../../Source/Board.cpp \
+                   ../../../../Source/Background.cpp \
+                   ../../../../Source/Bird.cpp \
+                   ../../../../Source/Column.cpp \
+                   ../../../../Source/Sound.cpp \
 
-LOCAL_SHARED_LIBRARIES := SDL2
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -lOpenSLES -llog -landroid
 
